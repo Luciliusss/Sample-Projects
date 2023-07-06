08ea651c4c92a7ab7b9ca7a99e22add3b5ce340f
tarık <trkpk77@gmail.com>
2023-07-06 14:47:47 +0300

    roulette: The roulette has been added

---
+++ roulette/roulette/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace roulette
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(388, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(206, 52);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(165, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1nd 12",
+            "2nd 12",
+            "3nd 12",
+            "All reds",
+            "All blacks",
+            "All Greens"});
+            this.comboBox1.Location = new System.Drawing.Point(388, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(261, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Amount for single digit play";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Amount for color or number range play";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(206, 132);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(165, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "The resulting number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Prediction";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(206, 78);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(165, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Amount of win/lose";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(206, 160);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(225, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(321, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Red: 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(334, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Black: 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(50, 240);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Green: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 404);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ roulette/roulette/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace roulette
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("lütfen sayı giriniz" , "System" , MessageBoxButtons.OK , MessageBoxIcon.Warning);
+            }
+            else
+            {
+                //System.Threading.Thread.Sleep(5000);
+
+                int color = 0;
+                int input1, input2, prediction, green = 0, cons = 0;
+                int[] red = { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+                int[] black = { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
+                
+                input1 = Convert.ToInt32(textBox1.Text);
+                input2 = Convert.ToInt32(textBox2.Text);
+                prediction = Convert.ToInt32(textBox4.Text);
+
+                Random random = new Random();
+                int number = random.Next(0, 37);
+                textBox3.Text = number.ToString();
+
+                if (number == green)
+                {
+                    color = 1;
+                }
+                else
+                {
+                    if (comboBox1.SelectedIndex == 3 || comboBox1.SelectedIndex == 4 || comboBox1.SelectedIndex == 5)
+                    {
+                        if (comboBox1.SelectedIndex == 3)
+                        {
+                            foreach (int i in red)
+                            {
+                                if (number == i)
+                                {
+                                    color = 2;
+                                }
+                            }
+                        }
+                        else if (comboBox1.SelectedIndex == 4)
+                        {
+                            foreach (int j in black)
+                            {
+                                if (number == j)
+                                {
+                                    color = 3;
+                                }
+                            }
+                        }
+                    }                   
+                }
+
+                if (textBox1.Text != "" || textBox2.Text != "" && textBox4.Text != "")                
+                {       
+                    if (comboBox1.SelectedIndex == 0 && number < 13)
+                    {
+                        if (prediction == number)
+                        {
+                            cons = input1 * 7;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }                   
+                        else if (prediction < 13 && prediction > 0 && prediction == number)
+                        {
+                            cons = (input1 + input2) * 7;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }
+                        else if (prediction < 13 && prediction > 0)
+                        {
+                            cons = input2 * 5;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Kazanamadınız");
+                            textBox1.Clear();
+                            textBox2.Clear();
+                            textBox3.Clear();
+                            textBox4.Clear();
+                            textBox5.Clear();
+                        }
+                    }
+                    else if (comboBox1.SelectedIndex == 1 && number > 12 && number < 25)
+                    {
+                        if (prediction == number)
+                        {
+                            cons = input1 * 7;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }
+                        else if (prediction < 25 && prediction > 12 && prediction == number)
+                        {
+                            cons = (input1 + input2) * 7;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }
+                        else if (prediction < 25 && prediction > 12)
+                        {
+                            cons = input2 * 5;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Kazanamadınız");
+                            textBox1.Clear();
+                            textBox2.Clear();
+                            textBox3.Clear();
+                            textBox4.Clear();
+                            textBox5.Clear();
+                        }
+                    }
+                    else if (comboBox1.SelectedIndex == 2 && number > 24 && number < 37)
+                    {
+                        if (prediction == number)
+                        {
+                            cons = input1 * 7;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }
+                        else if (prediction < 37 && prediction > 25 && prediction == number)
+                        {
+                            cons = (input1 + input2) * 7;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }
+                        else if (prediction < 37 && prediction > 25)
+                        {
+                            cons = input2 * 5;
+                            MessageBox.Show("tebrikler kazandınız");
+                            textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Kazanamadınız");
+                            textBox1.Clear();
+                            textBox2.Clear();
+                            textBox3.Clear();
+                            textBox4.Clear();
+                            textBox5.Clear();
+                        }
+                    }
+                    else if (comboBox1.SelectedIndex == 5 || comboBox1.SelectedIndex == 4 || comboBox1.SelectedIndex == 3 || color != 0)
+                    {
+                        if (number == prediction)
+                        {
+                            if (comboBox1.SelectedIndex == 5 && color == 1)
+                            {
+                                cons = (input1 + input2) * 10;
+                                MessageBox.Show("tebrikler kazandınız");
+                                textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                            }
+                            else if (comboBox1.SelectedIndex == 3 && color == 2)
+                            {
+                                cons = (input1 + input2) * 4;
+                                MessageBox.Show("tebrikler kazandınız");
+                                textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                            }
+                            else if (comboBox1.SelectedIndex == 4 && color == 3)
+                            {
+                                cons = (input1 + input2) * 4;
+                                MessageBox.Show("tebrikler kazandınız");
+                                textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Kazanamadınız");
+                                textBox1.Clear();
+                                textBox2.Clear();
+                                textBox3.Clear();
+                                textBox4.Clear();
+                                textBox5.Clear();
+                            }
+                        }
+                        else
+                        {
+                           if (comboBox1.SelectedIndex == 5 && color == 1)
+                           {
+                               cons = input2 * 10;
+                               MessageBox.Show("tebrikler kazandınız");
+                               textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                           }
+                           else if (comboBox1.SelectedIndex == 3 && color == 2)
+                           {
+                               cons = input2 * 4;
+                               MessageBox.Show("tebrikler kazandınız");
+                               textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                           }
+                           else if (comboBox1.SelectedIndex == 4 && color == 3)
+                           {
+                               cons = input2 * 4;
+                               MessageBox.Show("tebrikler kazandınız");
+                               textBox5.Text = ("tebrikler " + cons + " $ kazandınız");
+                           }
+                           else
+                           {
+                               MessageBox.Show("Kazanamadınız");
+                               textBox1.Clear();
+                               textBox2.Clear();
+                               textBox3.Clear();
+                               textBox4.Clear();
+                               textBox5.Clear();
+                           }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Kazanamadınız", "System" , MessageBoxButtons.OK, MessageBoxIcon.Information);                                            
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Kazanamadınız");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox3.Clear();
+                    textBox4.Clear();
+                    textBox5.Clear();
+                }
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
