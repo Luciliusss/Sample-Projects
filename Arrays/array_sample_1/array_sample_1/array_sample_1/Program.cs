6b444dcc582d394575bee8c545f8fa265e0f64b9
tarık <trkpk77@gmail.com>
2023-07-07 23:19:55 +0300

    folder: Sample of array has been added to 'Projects'

---
+++ Arrays/array_sample_1/array_sample_1/array_sample_1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Globalization;
+
+namespace array_sample_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] number = new int[5];
+            int j = 10;
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write("sayı gir : ");
+                j = Convert.ToInt32(Console.ReadLine());
+                number[i] = j;
+                Console.WriteLine(i + " numaralı array'a " + number[i] + " atandı");
+             }
+             */
+
+            /*------------------------------*/
+
+            /*
+            int[] numbers = { 1, 2, 3, 4, 5};
+            int newSize = numbers.Length +1;
+            int[] newNumbers = new int[newSize];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newNumbers[i] = numbers[i];
+            }
+
+            newNumbers[newSize - 1] = 6;
+            numbers = newNumbers;
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers[5]);
+            
+
+            /*------------------------------*/
+
+            /*
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[3]);
+            numbers[2] = 10;
+            Console.WriteLine(numbers[2]);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            */
+
+            /*------------------------------*/
+
+            /*
+            int[] numbers = { 5, 2, 1, 3, 4 };
+            Array.Sort(numbers);
+            
+            Console.WriteLine("dizi elemanlarının tersten sıralı hali:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            */
+
+            /*------------------------------*/
+
+            /*
+            int[] numbers = { 1, 2, 3, 4, 5};
+            Array.Reverse(numbers);
+            Console.WriteLine("Dizi elemanlarının tersten sıralı hali:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            */
+
+            /*------------------------------*/
+
+
+        }
+    }
+}
---
+++ Arrays/array_sample_2/array_sample_2/array_sample_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace array_sample_2
+{
+    internal class Program
+    {
+
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        static void Main()
+        {
+
+            Person[] people = new Person[3];
+            people[0] = new Person("Alice", 25);
+            people[1] = new Person("Bob", 35);
+            people[2] = new Person("Charlie", 35);
+
+            //
+            var selectedPeople = people.Where(p => p.Age > 30);
+
+            foreach (var person in selectedPeople)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            Console.WriteLine("");
+
+            //alphabetical name sorting
+            var sortedPeople = people.OrderBy(p => p.Name);
+
+            foreach (var person in sortedPeople)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            //age sorting, can't use both
+            /* 
+            var sortedPeople = people.OrderBy(p => p.Age);
+            
+            foreach (var person in sortedPeople)
+            {
+                Console.WriteLine(person.Age);
+            }*/
+
+
+        }
+
+    }
+}
