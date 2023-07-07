f7f5de0f4be70620a95a2fca3c254b2519f4e2f6
tarık <trkpk77@gmail.com>
2023-07-07 23:21:53 +0300

    folder: Sample of conditional statements has been added to projects

---
+++ Conditional_Statements/switch_case_sample_1/switch_case_sample_1/switch_case_sample_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace switch_case_sample_1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int dayofweek = (int)DateTime.Now.DayOfWeek;
+
+            switch (dayofweek)
+            {
+                case 1:
+                    Console.WriteLine("Today is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Today is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Today is Wendesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Today is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Today is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Today is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday");
+                    break;
+                default:
+                    Console.WriteLine("There is no day :)");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Conditional_Statements/switch_case_sample_2/switch_case_sample_2/switch_case_sample_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace switch_case_sample_2
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("Enter your age (to 'exit' to quit):");
+                string input = Console.ReadLine();
+
+                int age;
+                bool success = int.TryParse(input, out age);
+
+                if (input.ToLower() == "exit")
+                {
+                    exit = true;
+                    continue;
+                }
+
+                if (success)
+                {
+                    switch (age)
+                    {
+                        case int n when (n < 18):
+                            Console.WriteLine("Child");
+                            break;
+                        case int n when (n >= 18 && n < 30):
+                            Console.WriteLine("Youth");
+                            break;
+                        case int n when (n >= 30 && n <= 60):
+                            Console.WriteLine("adult");
+                            break;
+                        case int n when (n > 60):
+                            Console.WriteLine("Adult");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+            }
+        }
+    }
+
+}
